d0d327c971333fc49212bcd0e4b3def402118814
takumanaruke <leon32taku@gmail.com>
2018-08-10 15:15:19 +0900

    commit first application draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating cocktails...'
+Cocktail.create(name: "Rum soda")
+Cocktail.create(name: "Pinacorada")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create!(@cocktail)
+puts 'Finished!'
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
